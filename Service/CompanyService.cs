f82cba8c3c613465fa36bc528eac5d0a9274ccd3
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-02 13:48:50 +0500

    do some changes in service project and mappingProfile class

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Service
 {
@@ -14,19 +15,20 @@ namespace Service
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
 
-        public CompanyService(IRepositoryManager repository,ILoggerManager logger) 
+        public CompanyService(IRepositoryManager repository,ILoggerManager logger,IMapper mapper) 
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
         }
         public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
             try
             {
                 var companies = _repository.Company.GetAllCompanies(trackChanges);
-                var companiesDto = companies.Select(c =>new CompanyDto(c.Id, c.Name ?? "",
-                    string.Join(' ',c.Address,c.Country))).ToList();
+                var companiesDto =_mapper.Map<IEnumerable<CompanyDto>>(companies);
                 return companiesDto;
             }
             catch (Exception ex)
