b772202d66721708540c3cb8ce83b1f7272cafed
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-08 20:19:06 +0500

    remove useless classes (default classes) by name class1

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -5,6 +5,8 @@ using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+
 // Add services to the container.
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
@@ -17,12 +19,12 @@ builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
-
 var logger = app.Services.GetRequiredService<ILoggerManager>();
 app.ConfigureExceptionHandler(logger);
-
 if (app.Environment.IsDevelopment())
     app.UseHsts();
+
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
