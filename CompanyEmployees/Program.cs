ce970b8764bbc04a47ce1d12e1ae94251a85c54d
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-09 17:02:24 +0500

    added the alternative method of LoadConfiguration

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -5,7 +5,8 @@ using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
-
+// the above one is depricated now we can use below method also
+//LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Add services to the container.
 builder.Services.ConfigureRepositoryManager();
