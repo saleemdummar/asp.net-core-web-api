b0192caccebb4379c486b3a2534ae430544ef045
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-09 17:02:24 +0500

    added the alternative method of LoadConfiguration

--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -15,10 +15,10 @@ namespace LoggerService
 
         public void LogDebug(string message) => logger.Debug(message);
 
-        public void LogError(string message)=> logger.Error(message);
+        public void LogError(string message) =>logger.Error(message);
 
-        public void LogInfo(string message)=> logger.Info(message);
+        public void LogInfo(string message) =>logger.Info(message);
 
-        public void LogWarn(string message)=> logger.Warn(message);
+        public void LogWarn(string message) =>logger.Warn(message);
     }
 }
