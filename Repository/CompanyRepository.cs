5dbcba515116c2531644b4f65f4f35fdcc38bc6d
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-10 17:12:01 +0500

    created the post method for the company

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -14,6 +14,11 @@ namespace Repository
         {
         }
 
+        public void CreateCompany(Company company)
+        {
+            Create(company);
+        }
+
         public IEnumerable<Company> GetAllCompanies(bool trackChanges)
         {
             return FindAll(trackChanges).OrderBy(c => c.Name).ToList();
