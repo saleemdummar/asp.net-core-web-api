e4fdcb34a52ad7f408ba25912cd6d4407b2b86cb
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-14 17:41:23 +0500

    added  post mathod to createcompanyCollection and also to show companies from ids added get method

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -34,6 +34,23 @@ namespace Service
             return companyToReturn;
         }
 
+        public (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(
+            IEnumerable<CompanyForCreationDto> companyCollection)
+        {
+            if (companyCollection is null)
+                throw new CompanyCollectionBadRequest();
+            var companyEntities = _mapper.Map<IEnumerable<Company>>(companyCollection);
+            foreach (var company in companyEntities)
+            {
+                _repository.Company.CreateCompany(company);
+            }
+            _repository.Save();
+            var companyCollectionToReturn =
+            _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
+            var ids = string.Join(",", companyCollectionToReturn.Select(c => c.Id));
+            return (companies: companyCollectionToReturn, ids: ids);
+        }
+
         public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
            
