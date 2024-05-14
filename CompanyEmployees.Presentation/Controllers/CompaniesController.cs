e9881f916f5a580bc7f8ab3818fbad1ce29e2ef2
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-14 16:36:09 +0500

    Added new action in the controller name GetCompanyCollection

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -42,5 +42,12 @@ namespace CompanyEmployees.Presentation.Controllers
             var company = _service.CompanyService.CreateCompany(companyDto);
             return CreatedAtRoute("CompanyById", new { id = company.Id }, company);
         }
+
+        [HttpGet("collection/({ids})", Name ="CompanyCollection")]
+        public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+        {
+            var companies = _service.CompanyService.GetByIds(ids,trackChanges:false);
+            return Ok(companies);
+        }
     }
 }
