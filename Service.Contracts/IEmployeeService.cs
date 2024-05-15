14f96bf4bf7b437d96dce5fc936f425ffc05238f
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-15 16:18:24 +0500

    added the update method for the employee

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -14,5 +14,7 @@ namespace Service.Contracts
         EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto
              employeeForCreation, bool trackChanges);
         void DeleteEmployeeForCompany(Guid companyId, Guid Id, bool trackChanges);
+        void UpdateEmployeeForCompany(Guid companyId, Guid Id, EmployeeForUpdateDto
+                     employeeForUpdate, bool comTrackChanges,bool empTrackChanges);
     }
 }
---
+++ Shared/DataTransferObjects/EmployeeForUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DataTransferObjects
+{
+    // we do not need Id beacuse it will be accpted through URI , like in delete requests 
+    public record class EmployeeForUpdateDto(string Name,int Age,string position);
+}
