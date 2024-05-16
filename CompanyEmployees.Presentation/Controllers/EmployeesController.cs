bf661d5563d004b9168f36ab50d739f51c1f1135
Muhammad Saleem <saleemdummar@gmail.com>
2024-05-16 17:46:55 +0500

    install the newtonsoft json in presentation project and removed it from the main project for the validation of patch request beacuse without this method applyto function in the controller will error

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -75,9 +75,13 @@ namespace CompanyEmployees.Presentation.Controllers
             if (patchDoc is null)
                 return BadRequest("patchDoc object sent from client is null.");
             var result = _service.EmployeeService.GetEmployeeForPatch(companyId, id,
-            compTrackChanges: false,
-            empTrackChanges: true);
-            patchDoc.ApplyTo(result.employeeToPatch);
+            compTrackChanges: false, empTrackChanges: true);
+
+            patchDoc.ApplyTo(result.employeeToPatch,ModelState);
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+            
             _service.EmployeeService.SaveChangesForPatch(result.employeeToPatch,
             result.employeeEntity);
             return NoContent();
